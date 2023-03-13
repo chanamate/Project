a6d2d91e08306a58659f82fab7e1069bf37b15d9
chanamate <chanamate721.k@gmail.com>
2023-03-14 03:57:43 +0700

    14.35/03/23

--- src/utils/axios.instance.js
+++ src/utils/axios.instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    // baseURL: "http://20.2.233.46/api",
-    baseURL: "http://localhost:3000/api",
+    baseURL: "http://20.2.233.46/api",
+    // baseURL: "http://localhost:3000/api",
 
 })
 axiosInstance.interceptors.response.use(response => response.data.data)
